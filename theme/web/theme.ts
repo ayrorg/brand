a7ac2452f60d2c235603ad99b9d35dbc935dd618
Simen A. W. Olsen <so@bjerk.io>
2022-03-13 11:49:00 +0100

    add antializing

--- theme/web/theme.ts
+++ theme/web/theme.ts
@@ -1,4 +1,11 @@
 import { merge } from 'theme-ui';
 import { theme } from '../theme';
 
-export const webTheme = merge(theme, {});
+export const webTheme = merge(theme, {
+  styles: {
+    root: {
+      WebkitFontSmoothing: 'antialiased',
+      MozOsxFontSmoothing: 'grayscale',
+    },
+  },
+});
